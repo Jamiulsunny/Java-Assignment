15b5628874c3e13c3f48ed21772e902cd5152803
Jamiulsunny <jamiulsunny476@gmail.com>
2025-07-20 17:42:30 +0600

    change

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class EvenOdd {
+    public static void main (String[]args){
+
+        Scanner input = new Scanner (System.in);   
+        System.out.println ("Enter any positive integer: ");
+        num = input.nextInt();
+        if (num%2==0) {
+            System.out.println ("The number is even");
+        }
+        else{
+            System.out.println ("The number is odd");
+        }
+    }
+    
+}
