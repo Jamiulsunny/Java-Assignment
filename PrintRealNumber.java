c57b71480b289d6dad54a0be3403bb2af9b318bc
Jamiulsunny <jamiulsunny476@gmail.com>
2025-07-22 17:09:22 +0600

    realnumber

---
+++ PrintRealNumber.java
@@ -0,0 +1,9 @@
+
+public class PrintRealNumber {
+    public static void main(String[] args) {
+        System.out.println("Here are the real numbers:");
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
